01070e70e729b7298c8b67e06df61fa5e81133a7
Agustín Sánchez <dennot.sa@gmail.com>
2021-09-14 16:49:24 -0500

    last changes

--- final.py
+++ final.py
@@ -45,39 +45,48 @@ def get_reviews(link: str):
     
     driver = webdriver.Firefox(firefox_binary=binary, executable_path=r'/app/vendor/geckodriver/geckodriver', firefox_options=opts)
     
-    new_link = link.replace('dp', 'product-reviews')
-    re_link = re.findall(r'(.*)ref', new_link)
-    final_link = re_link[0]+'ref=cm_cr_dp_d_show_all_btm?ie=UTF8&reviewerType=all_reviews'
-    
-    driver.get(final_link)
+    if '/dp/' not in link:
+        new_link = re.sub(r'.com/(.p)?/', '.com/product-reviews/', link)
+    else:
+        new_link = link.replace('dp', 'product-reviews')    
+        re_link = re.findall(r'(.*)ref', new_link[0])
+        try:
+            final_link = re_link[0]+'ref=cm_cr_dp_d_show_all_btm?ie=UTF8&reviewerType=all_reviews'
+        except IndexError:
+            final_link = re.sub(r'(\?.*)', '/ref=cm_cr_dp_d_show_all_btm?ie=UTF8&reviewerType=all_reviews', new_link)
 
+    driver.get(final_link)
+    
     page_source = []
     
-    source_1 = driver.page_source
-    
+    source_1 = driver.page_source 
     
     world_selection = BeautifulSoup(source_1, 'lxml')
     total_selection = world_selection.find('div', class_='a-row a-spacing-base a-size-base')
-    total_reviews = re.findall(r'\| ([0-9]*)', str(total_selection))
-    total = total_reviews[0]   
+    total_reviews = re.findall(r'(?:\|) (.*)', str(total_selection))
+    reviews_numbers = re.sub(r'[^\d\.]', '', total_reviews[0])
+    total = reviews_numbers 
     
     while len(page_source) < (math.floor(int(total)/10)+.1):   
         
         try:
-            time.sleep(1)
+            time.sleep(.2)
             page_source.append(driver.page_source)
             driver.find_element_by_class_name('a-last').click()
             
         except WebDriverException:
             driver.quit()
+            driver.close()
             break
             
         except SessionNotCreatedException:
             driver.quit()
+            driver.close()
             break
         
         if len(page_source) >= (math.floor(int(total)/10)+.1):
             driver.quit()
+            driver.close()
             break
             
         
@@ -180,6 +189,8 @@ def word_cloud(dftext, stopwords_add):
     text = " ".join(review for review in dftext)
     stopwords = set(STOPWORDS)
     stopwords_add.append('br')
+    stopwords_add.append('Amazon')
+    stopwords_add.append('Basics')
     stopwords.update(stopwords_add)
     
     wordcloud = WordCloud(stopwords=stopwords, background_color='white').generate(text)
@@ -189,35 +200,71 @@ def word_cloud(dftext, stopwords_add):
     plt.axis('off')
     st.pyplot()
 
+title_placeholder = st.empty()
 st.set_option('deprecation.showPyplotGlobalUse', False)
-st.title('Amazon Review Analysis')
+st.image('titleamazon.png')
+show_side_lan = st.sidebar.selectbox('Language/Lenguaje', ('English','Español'))
 
-st.write('Made by Agustín Sánchez')
-st.write("This is a Python app that extracts text reviews from Amazon.com to analyze and create a meter that shows the overall sentiment (positive or negative) of the product's reviews.")
-st.write('To use it simply insert an Amazon.com (ONLY WORKS WITH LINKS FROM AMAZON US) product link in the bar below.')
-st.caption('Disclaimer: Due to Streamlit & Heroku limitations you might encounter that the app only extracted a fraction of the reviews.')
 
-user_input = st.text_input("Insert product link:")
-
-if len(user_input) != 0:
-    show_side = st.sidebar.selectbox('Show',('Review Analysis', 'Reviews Database'))
-    df = get_reviews(user_input)
-
-    if show_side == 'Review Analysis':
-        
-        stopwords_add = stopwords(user_input)
-        result, size, pos, nega = sentiment_scores(df['Body'])
-        st.write('We found', pos, 'positive reviews and', nega, 'negative reviews.')
-        score = (int(pos)/int(size))*100
-        img = math.floor(score/20)
-        st.image('tacometro_'+str(img)+'.png')
-        st.subheader('These are the most common words found within the reviews.')
-        word_cloud(df.Body, stopwords_add)
+if show_side_lan == 'English':
     
-    if show_side == 'Reviews Database':
-        
-        st.write('This is a dataframe of all the reviews extracted from your link.')
-        st.write(df)
-        
-    caching.clear_cache()
+    st.write('Made by Agustín Sánchez')
+    st.write("This is a Python app that extracts text reviews from Amazon.com to analyze and create a meter that shows the overall sentiment (positive or negative) of the product's reviews.")
+    st.write('To use it simply insert an Amazon.com (ONLY WORKS WITH LINKS FROM AMAZON US) product link in the bar below.')
+    st.caption('Disclaimer: Due to Streamlit & Heroku limitations you might encounter that the app only extracted a fraction of the reviews.')
 
+    user_input = st.text_input("Insert product link:")
+
+    if len(user_input) != 0:
+        
+        show_side = st.sidebar.selectbox('Show',('Review Analysis', 'Reviews Database'))
+        df = get_reviews(user_input)
+
+        if show_side == 'Review Analysis':
+        
+            stopwords_add = stopwords(user_input)
+            result, size, pos, nega = sentiment_scores(df['Body'])
+            st.write('We found', pos, 'positive reviews and', nega, 'negative reviews.')
+            score = (int(pos)/int(size))*100
+            img = math.floor(score/20)
+            st.image('tacometro_'+str(img)+'.png')
+            st.subheader('These are the most common words found within the reviews.')
+            word_cloud(df.Body, stopwords_add)
+    
+        if show_side == 'Reviews Database':
+        
+            st.write('This is a dataframe of all the reviews extracted from your link.')
+            st.write(df)
+        
+        caching.clear_cache()
+    
+if show_side_lan == 'Español':
+    
+    st.write('Hecho por Agustín Sánchez')
+    st.write("Esta es una aplicacion de Python que extrae reseñas de texto desde Amazon.com para analizarlas y crear un medidor que muestra el sentimiento general (positivo o negativo) de las reseñas del producto.")
+    st.write('Para usarlo simplemente inserta un link de un producto de Amazon.com (SOLO FUNCIONA CON LINKS DE AMAZON US) en la barra inferior.')
+    st.caption('Disclaimer: Debido a las limitaciones de Streamlit y Heroku es posible que la aplicacion extraiga solo una cantidad limitada de las reseñas del producto.')
+
+    user_input = st.text_input("Inserta el link del producto:")
+
+    if len(user_input) != 0:
+        show_side = st.sidebar.selectbox('Show',('Review Analysis', 'Reviews Database'))
+        df = get_reviews(user_input)
+
+        if show_side == 'Review Analysis':
+        
+            stopwords_add = stopwords(user_input)
+            result, size, pos, nega = sentiment_scores(df['Body'])
+            st.write('Encontramos', pos, 'reseñas positivas y', nega, 'reseñas negativas.')
+            score = (int(pos)/int(size))*100
+            img = math.floor(score/20)
+            st.image('tacometro_'+str(img)+'.png')
+            st.subheader('Estas fueron las palabras mas comunes encontradas dentro de las reseñas.')
+            word_cloud(df.Body, stopwords_add)
+    
+        if show_side == 'Reviews Database':
+        
+            st.write('Esta es una tabla de todas las reseñas extraidas.')
+            st.write(df)
+        
+        caching.clear_cache()
